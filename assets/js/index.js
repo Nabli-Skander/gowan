997633f870b205d9aad037a61cbfcc283ea3ba9b
Shibachou <xitid.gamer@gmail.com>
2020-06-19 15:46:01 +0200

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+$(window).on('load', function() {
+    // setTimeout(function () {
+    $('.loader').fadeOut('slow');
+    setTimeout(function () {
+        $('.content').fadeIn('slow');
+    }, 750)
+    // },3000);
+});
